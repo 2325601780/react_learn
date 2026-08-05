8460b3fd70addfa791827099f24e43f00484860c
曹一二 <15783362+cao-yiers@user.noreply.gitee.com>
2026-08-05 17:28:04 +0800

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//项目入口，从这里开始运行
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './day-01/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
